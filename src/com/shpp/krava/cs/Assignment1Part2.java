3aa68deaa78b4fd1d2da3044ed2a5157b5cfa17e
linxok <linxok@gmail.com>
2018-01-16 09:34:30 +0200

    move libs and others

--- src/com/shpp/krava/cs/Assignment1Part2.java
+++ src/com/shpp/krava/cs/Assignment1Part2.java
@@ -12,7 +12,7 @@
  * |>   1       1|
  * ...каждый "столб" должен стать цельным и содержать пять(в данном случае) камушков (биперов).
  * Вам стоит учесть, что финальное положение и направление взгляда Карела не важно, но тем не менее, нужно учитывать следующее:
- * 
+ *
  * Карел стартует в юго-западном углу и смотрит на восток, и у него полно биперов в рюкзаке;
  * колонны находятся на 1й, 5й, 9й строке и т.д., последняя колонна будет впритык к краю уровня;
  * колонн может быть много, а не 4 как в примере. Может, например, быть всего одна колонна (мир размером 1х8);
@@ -28,57 +28,75 @@ import com.shpp.karel.KarelTheRobot;
 public class Assignment1Part2 extends KarelTheRobot {
 
 
+    /*
+    move and build column while not end level.
+     */
     public void run() throws Exception {
+
         do {
 
-            buildColumn();
-            goBeck();
-            goNextColumn();
+            moveAndBuildColumn();
         } while (frontIsClear());
+        moveAndBuildColumn();
+
+    }
+
+    /*
+        decomposition : build column
+                        move to the root column
+                        move to the next column
+     */
+    private void moveAndBuildColumn() throws Exception {
+
         buildColumn();
-    }
+        moveToTheRootColumn();
+        moveToTheNextColumn();
 
-    private void goNextColumn() throws Exception {
-
-        move();
-        move();
-        move();
-        move();
 
     }
 
-    private void goBeck() throws Exception {
-        turnAround();
+    /*
+    move to the next column  if front is clear
+     */
+    private void moveToTheNextColumn() throws Exception {
+        if (frontIsClear()) {
+            move();
+            move();
+            move();
+            move();
+        }
+    }
+
+    /*
+    turn around and go to the root column and turn left
+     */
+    private void moveToTheRootColumn() throws Exception {
+        turnLeft();
+        turnLeft();
         while (frontIsClear()) {
             move();
         }
         turnLeft();
     }
 
-    private void turnAround() throws Exception {
-        turnLeft();
-        turnLeft();
-    }
+    /*
+    move in the column and put beeper if it no present
+     */
+
 
     private void buildColumn() throws Exception {
-        turnLeft();
 
+        turnLeft();
         while (frontIsClear()) {
-            ifBeepersNotPresentPutIt();
+            ifNoBeepersPutInThis();
             move();
         }
-        ifBeepersNotPresentPutIt();
+        ifNoBeepersPutInThis();
 
     }
 
-    private void ifBeepersNotPresentPutIt() throws Exception {
-        if (noBeepersPresent()) {
+    private void ifNoBeepersPutInThis() throws Exception {
+        if (noBeepersPresent())
             putBeeper();
-        }
     }
-
-
 }
-
-
-
